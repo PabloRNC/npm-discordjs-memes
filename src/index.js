2f6988e008d61bd7882d803cf2deb6b9ce665a62
Pablo Ruano <pabloruanonavarro@gmail.com>
2022-02-21 18:46:37 +0100

    npm

--- src/index.js
+++ src/index.js
@@ -33,7 +33,14 @@ function total() {
         "https://es.inviptus.com/blogadmin/wp-content/uploads/2015/09/IMG_9419-435x306.jpg",
         "https://es.inviptus.com/blogadmin/wp-content/uploads/2015/09/IMG_9420-435x422.jpg",
         "https://es.inviptus.com/blogadmin/wp-content/uploads/2015/09/IMG_9421-435x560.jpg",
-        "https://es.inviptus.com/blogadmin/wp-content/uploads/2015/09/IMG_9422-435x261.jpg"
+        "https://es.inviptus.com/blogadmin/wp-content/uploads/2015/09/IMG_9422-435x261.jpg",
+        "https://images3.memedroid.com/images/UPLOADED317/6212106801d1c.jpeg",
+        "https://images7.memedroid.com/images/UPLOADED846/621135f7645ad.jpeg",
+        "https://images7.memedroid.com/images/UPLOADED663/620182867eb21.jpeg",
+        "https://images3.memedroid.com/images/UPLOADED12/61fc0b381d0b2.jpeg",
+        "https://images7.memedroid.com/images/UPLOADED482/61f77f838cb15.jpeg",
+        "https://images7.memedroid.com/images/UPLOADED670/61f2f1acbbd87.jpeg",
+        "https://images3.memedroid.com/images/UPLOADED741/61e36c6e5bed9.jpeg"
 
     ]
    return memes[Math.floor(Math.random() * memes.length)]
@@ -51,6 +58,13 @@ function cinema(){
         "https://www.sdpnoticias.com/resizer/FxYQMKsOyd0JHBvdEM85MOtKQc8=/440x248/filters:format(png):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/sdpnoticias/B7JKSUHIDJECVFWCZH65VQHBPA.png",
         "https://as01.epimg.net/epik/imagenes/2018/03/05/portada/1520256687_631390_1520256766_noticia_normal.jpg",
         "http://images7.memedroid.com/images/UPLOADED572/60271a1036eda.jpeg",
+        "https://images3.memedroid.com/images/UPLOADED317/6212106801d1c.jpeg",
+        "https://images7.memedroid.com/images/UPLOADED663/620182867eb21.jpeg",
+        "https://images3.memedroid.com/images/UPLOADED12/61fc0b381d0b2.jpeg",
+        "https://images7.memedroid.com/images/UPLOADED482/61f77f838cb15.jpeg",
+        "https://images7.memedroid.com/images/UPLOADED670/61f2f1acbbd87.jpeg",
+        "https://images3.memedroid.com/images/UPLOADED741/61e36c6e5bed9.jpeg"
+        
         
     ]
     return pmemes[Math.floor(Math.random() * pmemes.length)]
